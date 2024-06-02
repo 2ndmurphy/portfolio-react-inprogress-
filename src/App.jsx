48f2de26d3622e8bf90e673843bec51a1ee3a7cd
unknown <2ndmurphy>
2024-06-01 21:16:41 -0700

    [image]

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ImgMe from "./assets/img/me.png"
 
 function About() {
   return (
@@ -7,18 +8,32 @@ function About() {
       <div className="w-full flex flex-col gap-4 bg-white 
         justify-start md:flex-nowrap px-2 py-3
         rounded-lg shadow-md">
-        <h1 className="text-xl md:text-2xl text-start">ABOUT ME</h1>
+        <h1 className="text-xl md:text-2xl text-center">ABOUT ME</h1>
         <div className="w-auto flex flex-wrap md:flex-nowrap overflow-hidden bg-orange-100 rounded-md p-2">
-          <div className='w-full md:w-1/2 bg-slate-500'>
+          <div className='w-full md:w-1/2 bg-slate-500 flex justify-center items-center px-2'>
             <p className="text-start text-base-500">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+              Ut laboriosam velit repellendus eaque sint eligendi aspernatur.
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
               Ut laboriosam velit repellendus eaque sint eligendi aspernatur.
             </p>
           </div>
-          <div className='bg-black'>
-            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" width={350} height={350}>
+          <div className='relative' style={{ width: 360, height: 360 }}>
+            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" width={360} height={360}>
               <path fill="#FF0066" d="M46,-31.1C58.4,-21.1,66.6,-2,62.8,14.3C59,30.6,43.4,44.1,24.8,54.1C6.2,64,-15.4,70.4,-24.2,61.8C-33.1,53.1,-29.1,29.4,-31.6,9.7C-34.1,-10,-43,-25.7,-38.8,-33.8C-34.6,-42,-17.3,-42.7,-0.3,-42.5C16.8,-42.3,33.5,-41.2,46,-31.1Z" transform="translate(100 100)" />
             </svg>
+            <img
+              className='absolute'
+              src={ImgMe} 
+              alt="..." 
+              width={150} 
+              height={150}
+              style={{
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+              }}
+            />
           </div>
         </div>
       </div>
@@ -127,7 +142,7 @@ function App() {
                 </div>
               </div>
             
-              <div className="max-w-2xl mx-auto bg-white mt-4 rounded-b-lg shadow-md px-2">
+              <div className="max-w-2xl mx-auto bg-white mt-4 rounded-b-lg shadow-md px-4">
                 <div className="text-sm breadcrumbs">
                   <ul>
                     <li>
@@ -161,7 +176,7 @@ function App() {
         
           <div className="drawer-side">
             <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
-            <div className="menu p-4 w-80 min-h-full text-base-content bg-transparent">
+            <div className="menu p-4 w-80 min-h-full text-base-content bg-orange-100">
               {/* Sidebar content here */}
               <div className='w-full h-64 p-3 bg-white rounded-box'>
                 <div className='flex justify-start items-center gap-3 py-3 border-b-2'>
